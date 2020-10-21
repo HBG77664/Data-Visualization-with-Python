92ad0d3711d1f658b0b0bee49c3e35860919c266
HEM <71067957+HBG77664@users.noreply.github.com>
2020-10-21 16:29:59 +0545

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+  # create the dataframe using the json file# create the dataframe using the json file# create the dataframe using the json file# create the dataframe using the json file# create the dataframe using the json file
+df = pd.read_json(r'./rain.json')
+
+plt.figure(figsize=(15,5))
+plt.plot(df['Month'],df['Temperature'], label='Temperature')
+#plt.show()
+
+# plot rainfall
+plt.figure(figsize=(17,5))
+plt.plot(df['Month'],df['Rainfall'], label='Rainfall')
+#plt.show()
+
+plt.plot(df['Month'],df['Rainfall'], label='Rainfall')
+plt.plot(df['Month'],df['Temperature'], label='Temperature')
+plt.legend()
+plt.show()
+
